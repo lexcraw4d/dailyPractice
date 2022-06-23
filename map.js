efdf9f5ec0b664bb97946c1367ec51f06bb48dd5
Lex Crawford <lexcraw4d@gmail.com>
2022-06-22 23:19:47 -0400

    first commit

---
+++ map.js
@@ -0,0 +1,48 @@
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: '202',
+        mass: '136',
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: '150',
+        mass: '49',
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: '188',
+        mass: '84',
+        eye_color: 'blue',
+        gender: 'male',
+    }
+];
+//Map iterate through each character and transform item in some way --> returns another array resulting from previous arrays
+const names = characters.map((character) => {
+    return character.name
+})
+// console.log(names)
+const heights = characters.map((character) => character.height)
+// console.log(heights)
+
+const minifiedRecords = characters.map(character => ({ 
+    name: character.name,
+    height: character.height
+}))
+// console.log(minifiedRecords)
+
+const firstNames = characters.map((character) => 
+    character.name.split(' ')[0]
+)
+// console.log(firstNames)
