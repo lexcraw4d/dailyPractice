31ecd3ba12c84607b3e3e955f8f6620a721a4aff
Lex Crawford <lexcraw4d@gmail.com>
2022-09-18 02:25:10 -0400

    adding documentation on how to use better code comments for future reference

---
+++ commentExample.js
@@ -0,0 +1,8 @@
+// ! This is a very critical comment
+// ? This is a question comment
+// TODO: This is a todo comment
+// * This is a highlighted comment
+// This is a normal comment
+
+
+//* can update more settings in config.json file
